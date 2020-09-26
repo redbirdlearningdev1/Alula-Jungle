c120dde10174df47a9d28cfd0458769b13aa16ce
Marco Ravelo <mravelo5874@tamu.edu>
2020-09-25 19:49:25 -0500

    work on outline shader

--- JungleGame/Assets/Scripts/Tools/MapIcon.cs
+++ JungleGame/Assets/Scripts/Tools/MapIcon.cs
@@ -1,22 +1,21 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-public class MapIcon : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+public class MapIcon : MonoBehaviour, IPointerClickHandler
 {
-    //private Outline outlineComp;
-
+    public string connectedScene;
+    private MeshRenderer meshRenderer;
     private static float pressedScaleChange = 0.95f;
-
-    public Color hoverOverColor;
-    public Color noHoverColor;
 
     void Awake() 
     {
-        //outlineComp = GetComponent<Outline>();
-        //outlineComp.effectColor = noHoverColor;
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    public void SetOutineColor(Color color)
+    {
+        meshRenderer.material.color = color;
     }
 
     public void OnPointerClick(PointerEventData eventData)
@@ -26,20 +25,10 @@ public class MapIcon : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler,
 
     private IEnumerator OnPressScale(float duration)
     {
-        GameHelper.NewLevelPopup(new Level("DevMenu"));
+        GameHelper.NewLevelPopup(new Level(connectedScene));
 
         transform.localScale = new Vector3(pressedScaleChange, pressedScaleChange, 1f);
         yield return new WaitForSeconds(duration);
         transform.localScale = new Vector3(1f, 1f, 1f);
-    }
-
-    public void OnPointerEnter(PointerEventData eventData)
-    {
-        //outlineComp.effectColor = hoverOverColor;
-    }
- 
-    public void OnPointerExit(PointerEventData eventData)
-    {
-        //outlineComp.effectColor = noHoverColor;
     }
 }
